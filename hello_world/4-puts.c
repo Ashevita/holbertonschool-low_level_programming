b06e7d0399cef2d71a01c3cfe8be6115f1fa641a
root <you@example.com>
2024-06-17 13:58:02 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main(void)
+/* comment */
 {
-puts("\"Programming is like building a multilingual puzzle"); 
+
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}	
+}
