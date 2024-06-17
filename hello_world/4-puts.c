6a6d10b2dd9ae3249f95fb8f8de0a271f3837e35
root <you@example.com>
2024-06-17 14:08:44 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(void)
 /**
- * main - Entry point
- *
- * Description: 'the program's description'
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
 {
 
 puts("\"Programming is like building a multilingual puzzle");
