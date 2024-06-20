064c85dc5766b318649a1482cf7109a2d53f4015
root <you@example.com>
2024-06-20 13:41:26 +0000

    putchar

---
+++ functions_nested_loops/_putchar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int _putchar(void)
+{
+
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+
+
+{
+}
+putchar('\n');
+
+return (0);
+}
