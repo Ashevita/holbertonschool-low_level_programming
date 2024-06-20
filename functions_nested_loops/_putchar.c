71277c7ebfecfe6743fea5f9fe1aa245f97523f5
root <you@example.com>
2024-06-20 14:31:12 +0000

    putchar

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,28 +1,13 @@
-#include <stdio.h>
-#include "main.h"
+#include <unistd.h>
+
 /**
- * main - Entry point
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Description: 'the program's description'
- *
- * Return: Always 0 (Success)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(void)
+int _putchar(char c)
 {
-
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-
-
-{
-}
-putchar('\n');
-
-return (0);
+	return (write(1, &c, 1));
 }
