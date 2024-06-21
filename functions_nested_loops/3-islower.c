028327dad0882ad769a986dc346559e66078ba72
root <you@example.com>
2024-06-21 11:23:29 +0000

    lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * Descritpion: check if a lowercase
+ *
+ *
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
