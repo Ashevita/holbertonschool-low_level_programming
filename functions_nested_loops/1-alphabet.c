b16e753eddbdaab866f470063326435dc822b976
root <you@example.com>
2024-06-21 08:45:29 +0000

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+        char c;
+
+        for (c = 'a'; c <= 'z'; ++c)
+{
+        _putchar(c);
+}
+
+_putchar('\n');
+
+}
