ee3c374d0297eddb00cc0565ebe946fdeb87b1cb
root <you@example.com>
2024-06-23 10:49:16 +0000

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _abs - print the absolute number
+*
+*@x: character to be verifie
+*Return: return x or -x
+*/
+
+
+
+
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		return (-x);
+	}
+else
+{
+		return (x);
+	}
+}
