4e887d886a61c6d2c4b8ec603d6e92857b63c638
root <you@example.com>
2024-06-24 08:34:48 +0000

    isdigit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+*_isdigit - controls if a character is in lowercase
+*@c: character to be verified
+*Return: return 0 or 1
+*/
 
 int _isdigit(int c)
 {
