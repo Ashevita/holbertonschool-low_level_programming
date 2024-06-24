132b1520cb920834e277c3e6b556edc009cca1bf
root <you@example.com>
2024-06-24 12:19:44 +0000

    .

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*more_numbers - prints the alphabet 10 fois
+*
+* Description: Prints the alphabet from 'a' to 'z' followed by a newline.
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+	{
+			if (i >= 10)
+
+	{
+			_putchar('1');
+	}
+		_putchar(j % 10 + '0');
+	}
+	_putchar('\n');
+}
+}
+
