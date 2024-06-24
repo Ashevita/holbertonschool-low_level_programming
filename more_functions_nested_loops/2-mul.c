d68afaaf0f0c69202347d97af565ccc774ca3902
root <you@example.com>
2024-06-24 08:49:48 +0000

    mul

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "main.h"
-
-
-
-
-
-
+/**
+* mul - mul two values
+* @a: The first number to be computed
+* @b: The second number to be computed
+*
+* Return: Integer value
+*/
 int mul(int a, int b)
 {
 	return (a * b);
