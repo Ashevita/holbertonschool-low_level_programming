14495c6c0e000429a9be87b562ea81194b9d0418
root <you@example.com>
2024-06-24 09:07:12 +0000

    .

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_numbers - controls if a character is in lowercase
+*
+*/
+void print_numbers(void)
+{
+char c;
+	for (c = '0'; c <= '9'; ++c)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
