ae0800cc8e7221ebaffc0e092024b57ebf03b7e5
root <you@example.com>
2024-06-24 09:26:30 +0000

    .

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+*print_most_numbers - prints most number
+*
+* Description: Prints number but no 2 and 4
+*/
 void print_most_numbers(void)
 {
 	char c;
-		for (c = '0'; c <= '9'; c++)
+
+	for (c = '0'; c <= '9'; c++)
 {
 	if (c != '2' && c != '4')
 	{
