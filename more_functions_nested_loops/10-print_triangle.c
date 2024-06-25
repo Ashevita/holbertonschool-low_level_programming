f3c27ba4ebc3cb00af35fda0ccc70099bfa5ec73
root <you@example.com>
2024-06-25 13:26:15 +0000

    .

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * print_triangle - print a triangle
+ *@size: size of triangle
+ */
 void print_triangle(int size)
 {
 	int i;
 	int j;
-	
+	int k;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+
+			for (k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
