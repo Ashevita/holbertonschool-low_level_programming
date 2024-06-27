a68dd50541219a9568b04dc171d691111e2bbde4
root <you@example.com>
2024-06-27 08:27:25 +0000

    .

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - puts
+ * @str: cariable
+ */
+
+void print_rev(char *s)
+{
+	int a = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+	}
