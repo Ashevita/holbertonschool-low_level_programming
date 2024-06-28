5a82f00aee6d619e01c6dd909fdf7f6572d20684
root <you@example.com>
2024-06-28 09:11:57 +0000

    .

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - print number
+ * @i: variable
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n -1)
+		{
+			printf(", ");
+		}
+	}
+
+	printf("\n");
+}
