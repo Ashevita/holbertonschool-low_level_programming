472b799e06d518c2642e1d0d1b4ee299d42c4558
Ashevita <you@example.com>
2024-07-01 13:03:53 +0000

    .

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - function
+ * @dest: pointeur
+ * @src: pointeur
+ * @n: pointeur
+ * Return: char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+        char *p = dest;
+
+        while (n--)
+        {
+	*p++ = *src++;
+        }
+
+        return (dest);
+}
