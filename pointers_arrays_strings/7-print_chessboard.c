9d8d2298729981130023426bcb3e7315a46074c9
root <you@example.com>
2024-07-02 21:50:47 +0000

    print_chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,8 @@
 #include "main.h"
+/**
+ * print_chessboard - function*
+ * @a: variable
+ */
 
 void print_chessboard(char (*a)[8])
 {
@@ -9,7 +13,7 @@ void print_chessboard(char (*a)[8])
 	for (j = 0; j < 8; j++)
 	{
 		_putchar(a[i][j]);
-	 }
+	}
 	_putchar('\n');
 	}
 }
