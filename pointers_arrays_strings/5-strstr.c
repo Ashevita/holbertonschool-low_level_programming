778aa0af925a5869b4cca3880bfe81031318232f
root <you@example.com>
2024-07-02 14:14:40 +0000

    .

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,32 @@
 #include "main.h"
+/**
+ * _strstr - function
+ */
 
 char *_strstr(char *haystack, char *needle)
 {
-	return ;
+	if (*needle == '\0')
+	{
+		return (haystack);
+}
+
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+	while (*h != '\0' && *n != '\0' && *h == *n)
+	{
+		h++;
+		n++;
+	}
+
+	if (*n == '\0')
+	{
+		return (haystack);
+	}
+
+		haystack++;
+}
+	return ('\0');
 }
