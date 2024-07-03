ca8df0c26b73bc41ca4f8f34e16ac5df9eeeba50
root <you@example.com>
2024-07-03 09:03:30 +0000

    print_diagsums

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagsums - function
+ * @a: variable
+ * @size: variable
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum1, sum2 = 0;
+	int i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+	sum1 += a[i * size + i];
+	sum2 += a[i * size + (size - 1 - i)];
+	}
+
+		printf("%d, %d\n", sum1, sum2);
+}
