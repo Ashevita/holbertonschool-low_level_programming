c916d7aaaa822761dcfb70b711e34c46afcd8e41
root <you@example.com>
2024-07-03 12:10:58 +0000

    print_rev_recursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,15 +1,14 @@
 #include "main.h"
+/**
+ * _print_rev_recursion - function
+ * @s: variable
+ */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
-		return;
-	}
-	else
-	{
-		_putchar(*s);
 		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
