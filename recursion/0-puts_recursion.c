376a0e4927e0aadd63d5f4be8cfb0207cbc3050f
root <you@example.com>
2024-07-03 12:00:06 +0000

    puts_recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,17 +1,22 @@
 #include "main.h"
 /**
- * _puts_recursion - function
- * @s: variable
+ * _puts_recursion - Fonction qui imprime une chaîne
+ * de caractères puis un retou la ligne.
+ * @s: Pointeur vers la chaîne de caractèr imprimer.
+ *
+ * Description: Cette fonction imprime chaque caractère de la chaîne
+ * pointée par 's' de manière récursive. Lorsqu'elle atte
+ * le caractère nul ('\0'), elle imprime un retou la ligne
+ * et termine la récursio
+ *
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+if (*s == '\0')
 	{
-		_putchar('\n');
-		return;
+	_putchar('\n');
+	return;
 	}
-
-		_putchar(*s);
-		_puts_recursion(s + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
