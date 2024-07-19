d55f45dc7d311f4d690addb822f4558be472a3ff
root <you@example.com>
2024-07-19 09:18:41 +0000

    ex0p2

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	va_list args;
+	unsigned int i = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+		}
+
+	va_end(args);
+
+	return (sum);
+}
