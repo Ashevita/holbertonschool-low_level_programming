22aba7e63d9fab51a98e570f869a7082392043ba
root <you@example.com>
2024-08-02 09:06:42 +0000

    add node end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -8,37 +8,39 @@
  * @str: pointer
  * Return: nomber of a node
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
-	list_t *temp = *head;
+	list_t *new_node;
+	list_t *temp;
 
-		if (new_node == NULL)
-		{
-			return (NULL);
-		}
-		new_node->str = strdup(str);
-		if (new_node->str == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
+	char *new_str;
+	int len;
 
-        new_node->next = NULL;
+	new_str = strdup(str);
+	if (new_str == NULL)
+	return (NULL);
 
-        if (*head == NULL)
-        {
-            *head = new_node;
-        }
-        else
-        {
-            while (temp->next != NULL)
-            {
-                temp = temp->next;
-            }
-            temp->next = new_node
-            
-        }
-    return(new_node);
+	for (len = 0; str[len]; len++)
+		;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(new_str);
+	return (NULL);
+	}
+		new_node->str = new_str;
+		new_node->len = len;
+		new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+		temp->next = new_node;
+	}
+	return (new_node);
 }
