0913ccfc752ca78b224dbdff8dc12ebf095382e1
Ashevita <9362@holbertonstudents.com>
2024-08-02 12:06:27 +0200

    free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void free_list(list_t *head)
+{
+list_t *temp
+
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp->str);
+free(tmp);
+}
+}
