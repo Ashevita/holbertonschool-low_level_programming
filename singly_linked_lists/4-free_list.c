a2f6d2218e58405f6e351bf21f8597456d455cc0
root <you@example.com>
2024-08-02 10:16:29 +0000

    free lists

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -5,13 +5,13 @@
 
 void free_list(list_t *head)
 {
-list_t *temp
+	list_t *temp;
 
 while (head != NULL)
 {
-		temp = head;
-		head = head->next;
+temp = head;
+head = head->next;
 free(temp->str);
-free(tmp);
+free(temp);
 }
 }
