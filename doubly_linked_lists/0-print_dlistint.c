2d92f25c57b6073965ba99d1da97d26842f736c1
root <you@example.com>
2024-08-04 12:58:52 +0000

    print dlistint

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,17 @@
 #include "lists.h"
-
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count = 0
+	size_t count = 0;
 
 		while (h != NULL)
 		{
-			printf("%d,\n", h->n);
+			printf("%d\n", h->n);
 			count ++;
-			h = h->next
+			h = h->next;
 		}
 	return (count);
 }
