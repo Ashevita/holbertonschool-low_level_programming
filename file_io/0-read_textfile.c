bca162ab858300ddad2b43f9658cf3147c171795
root <you@example.com>
2024-08-04 11:53:35 +0000

    0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes_read, bytes_written;
+	size_t total_bytes_written = 0;
+	char ch;
+
+	if (filename == NULL)
+	return (0);
+		fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	return (0);
+	while (letters > 0 && (bytes_read = read(fd, &ch, 1)) > 0)
+	{
+		bytes_written = write(STDOUT_FILENO, &ch, 1);
+	if (bytes_written != 1)
+	{
+		close(fd);
+	return (0);
+	}
+		total_bytes_written += bytes_written;
+		letters--;
+	}
+	if (bytes_read == -1)
+	{
+		close(fd);
+	return (0);
+	}
+		close(fd);
+	return (total_bytes_written);
+}
