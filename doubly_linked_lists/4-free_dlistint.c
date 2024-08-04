7c58bbdbab42dbaddd091ef430c9643dab7d52c3
root <you@example.com>
2024-08-04 18:58:25 +0000

    free

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - Libère une liste doublement chaînée
+ * @head: Pointeur vers le début de la liste
+ */
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *current;
+    dlistint_t *next;
+
+    current = head;
+
+    while (current != NULL)
+    {
+        next = current->next;
+	printf("Freeing node at address: %p\n", (void*)current);
+        free(current);
+        current = next;
+    }
+}
